b66f007cfee837814004554fdb832f1e6066f60d
wifisam <wilfredoalasmelendez@yahoo.com>
2025-01-17 14:19:23 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ruta de tipo resource 
+Route::resource('almacen/categoria', 'categoriaController');
