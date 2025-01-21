88953faf49db46025b4e724a351272639894842a
wifisam <wilfredoalasmelendez@yahoo.com>
2025-01-20 19:40:06 -0600

    aplicacion de plantilla de adminlte

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', function () {
 
 // ruta de tipo resource 
 Route::resource('almacen/categoria', 'categoriaController');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
